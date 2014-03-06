52801963f56faa27638a8374071d6c9190e5e37c
Steffen Kaempke <steffen.kaempke@hypoport.de>
2014-03-06 18:24:02 +0100

    add log and main stuff

--- README.md
+++
@@ -1,4 +0,0 @@
-hypo-caching-rest
-=================
-
-Rest backand for the hypoport caching app 
---
+++ src/main/java/de/vergleich/versicherung/service/inno/Main.java
@@ -0,0 +1,11 @@
+package de.vergleich.versicherung.service.inno;
+
+public class Main {
+
+  public static void main(String[] args) {
+   
+  }
+
+
+
+}
