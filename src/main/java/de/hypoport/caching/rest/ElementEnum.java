c732e72d44d1731fa33c5b34a9022f158033f4c1
Steffen Kaempke <steffen.kaempke@hypoport.de>
2014-03-07 12:30:51 +0100

    add an element array e.g. BAUM & FUNDAMENT

---
+++ src/main/java/de/hypoport/caching/rest/ElementEnum.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hypoport.caching.rest;
+
+/**
+ *
+ * @author steffen.kaempke
+ */
+public enum ElementEnum {
+
+  NICHTS,
+  FUNDAMENT,
+  ROHBAU,
+  ROHBAU_DACH,
+  BAUM;
+}
